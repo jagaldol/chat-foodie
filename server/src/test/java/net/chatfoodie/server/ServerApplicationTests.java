b0dd99b1da45653ff6e4bb987b19a02fdaa78c81
HYEJUN <yoy06056@pusan.ac.kr>
2023-05-30 17:18:46 +0900

    init(server): Init server spring boot project
    use java 17

---
+++ server/src/test/java/net/chatfoodie/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.chatfoodie.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
