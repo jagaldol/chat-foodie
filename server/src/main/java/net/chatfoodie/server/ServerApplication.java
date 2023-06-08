59c21d616f82188a096d05e587d2564cf8dd613f
jagaldol <yoy06056@pusan.ac.kr>
2023-06-08 13:24:00 +0900

    feat: Implement dbms Setup file Autogeneration Logic
    - 실행 시 applicaiton-settings.properties를 생성
    - properties에 mysql connection 정보를 입력하여 서버에 dbms 연결 가능

--- server/src/main/java/net/chatfoodie/server/ServerApplication.java
+++ server/src/main/java/net/chatfoodie/server/ServerApplication.java
@@ -3,11 +3,60 @@ package net.chatfoodie.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.*;
+import java.util.Properties;
+
 @SpringBootApplication
 public class ServerApplication {
 
+    final static private String RESOURCE_PATH = "src/main/resources/";
+
+    final static private String CONFIG_FILE = "application-settings.properties";
+
     public static void main(String[] args) {
+        if (!confirmSettingProperties())
+            return;
+
         SpringApplication.run(ServerApplication.class, args);
     }
 
+    private static Boolean confirmSettingProperties() {
+        var settingProperties = new File(RESOURCE_PATH + CONFIG_FILE);
+        if (settingProperties.exists()) {
+            return true;
+        }
+        System.out.println("application-settings.properties 파일을 생성합니다.");
+        createSettingProperties(settingProperties);
+        System.out.println("application-setting.properties 를 수정하고 다시 실행해 주세요.");
+        return false;
+
+    }
+
+    private static void createSettingProperties(File propertiesFile) {
+        Properties prop = new Properties();
+        OutputStream output = null;
+
+        try {
+            output = new FileOutputStream(propertiesFile);
+
+            prop.setProperty("spring.datasource.url", "jdbc:mysql://{your server address}:3306/{your table name}?useSSL=false&characterEncoding=UTF-8&serverTimezone=UTC");
+            prop.setProperty("spring.datasource.username", "{your username}");
+            prop.setProperty("spring.datasource.password", "{your password}");
+
+            // save properties to project root folder
+            prop.store(output, null);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (output != null) {
+                try {
+                    output.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
 }
