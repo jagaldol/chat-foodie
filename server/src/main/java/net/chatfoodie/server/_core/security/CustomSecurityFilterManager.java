eaf1b985d584322c206619ec344ce5448010834a
jagaldol <yoy06056@pusan.ac.kr>
2023-07-21 17:41:28 +0900

    feat: JWT 인증 필터 추가

--- server/src/main/java/net/chatfoodie/server/_core/security/CustomSecurityFilterManager.java
+++ server/src/main/java/net/chatfoodie/server/_core/security/CustomSecurityFilterManager.java
@@ -1,11 +1,14 @@
 package net.chatfoodie.server._core.security;
 
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 public class CustomSecurityFilterManager extends AbstractHttpConfigurer<CustomSecurityFilterManager, HttpSecurity> {
     @Override
     public void configure(HttpSecurity builder) throws Exception {
+        AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+        builder.addFilter(new JwtAuthenticationFilter(authenticationManager));
         super.configure(builder);
     }
 }
