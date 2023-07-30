67e00719c690aae137b37cf73548d9feb41b6f4e
jagaldol <yoy06056@pusan.ac.kr>
2023-07-30 11:57:55 +0900

    feat: Duplicate Check of Id and email and Exception Handling
    - 트랜잭션을 부여하여 단위 실행을 보장
    - 각 조회 후 중복 시 Bad Request 전달
    - 통과했는데 에러 발생(e.g. Phantom Read) 시 internal server error 리턴

--- server/src/main/java/net/chatfoodie/server/user/service/UserService.java
+++ server/src/main/java/net/chatfoodie/server/user/service/UserService.java
@@ -2,14 +2,18 @@ package net.chatfoodie.server.user.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.chatfoodie.server._core.errors.exception.Exception400;
+import net.chatfoodie.server._core.errors.exception.Exception500;
 import net.chatfoodie.server._core.security.JwtProvider;
 import net.chatfoodie.server.user.dto.UserRequest;
 import net.chatfoodie.server.user.User;
 import net.chatfoodie.server.user.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class UserService {
@@ -19,11 +23,27 @@ public class UserService {
     final private PasswordEncoder passwordEncoder;
 
 
+    @Transactional
     public void join(UserRequest.JoinDto requestDto) {
-        // TODO: 중복 체크 필요
-        log.debug(requestDto.toString());
+        var loginId = requestDto.loginId();
+        var email = requestDto.email();
+
+
+        if (userRepository.findByLoginId(requestDto.loginId()).isPresent()) {
+            throw new Exception400("이미 존재하는 아이디입니다");
+        }
+
+        if (userRepository.findByEmail(requestDto.email()).isPresent()) {
+            throw new Exception400("이미 존재하는 이메일입니다.");
+        }
+
         String encodedPassword = passwordEncoder.encode(requestDto.password());
-        userRepository.save(requestDto.createUser(encodedPassword));
+
+        try {
+            userRepository.save(requestDto.createUser(encodedPassword));
+        } catch (Exception e) {
+            throw new Exception500("회원가입 중에 오류가 발생했습니다. 다시 시도해주세요.");
+        }
     }
 
     public String issueJwtByLogin(UserRequest.LoginDto requestDto) {
