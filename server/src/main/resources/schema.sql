0638c017e658ad0e37d3100c7862977c8155b2be
jagaldol <yoy06056@pusan.ac.kr>
2023-07-30 22:56:38 +0900

    chore: Change  Database Initialization setting
    - schema.sql과 data.sql을 production level에서는 실행 안하도록 변경 / local 세팅에서만 실행
    - data.sql 추가하여 food 기본 정보 초기화 할 예정

--- server/src/main/resources/schema.sql
+++ server/src/main/resources/schema.sql
@@ -1,36 +1,10 @@
-CREATE TABLE IF NOT EXISTS user (
-    id INT AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS user_tb (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     login_id VARCHAR(40) NOT NULL,
-    password VARCHAR(64) NOT NULL,
+    password VARCHAR(100) NOT NULL,
     name VARCHAR(20) DEFAULT '회원',
-    gender TINYINT(1) DEFAULT 0,
-    birth DATE DEFAULT (CURRENT_DATE),
+    gender TINYINT(1) DEFAULT false,
+    birth DATE DEFAULT '2000-01-01',
     email VARCHAR(100),
-    created_at TIMESTAMP DEFAULT now(),
-    CONSTRAINT user_id_uindex PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS admin (
-    id INT AUTO_INCREMENT,
-    login_id VARCHAR(40) NOT NULL,
-    password VARCHAR(64) NOT NULL,
-    name VARCHAR(20) DEFAULT '관리자',
-    created_at TIMESTAMP DEFAULT now(),
-    CONSTRAINT admin_id_uindex PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS chatroom (
-    id INT AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT now(),
-    CONSTRAINT chatroom_id_uindex PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS message (
-    id INT AUTO_INCREMENT,
-    chatroom_id INT NOT NULL,
-    from_user_id INT NOT NULL,
-    content TEXT,
-    created_at TIMESTAMP DEFAULT now(),
-    CONSTRAINT message_id_uindex PRIMARY KEY (id)
+    created_at TIMESTAMP DEFAULT now()
 );
