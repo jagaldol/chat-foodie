72df2a9b8416f256d736c324dfb97c84404a1212
icarus012832 <icarus012832@gmail.com>
2023-08-02 17:27:03 +0900

    feat: Add apiutils at food preference
    apiutils 추가

--- server/src/main/java/net/chatfoodie/server/food/controller/FoodController.java
+++ server/src/main/java/net/chatfoodie/server/food/controller/FoodController.java
@@ -2,17 +2,11 @@ package net.chatfoodie.server.food.controller;
 
 import lombok.RequiredArgsConstructor;
 import net.chatfoodie.server._core.utils.ApiUtils;
-import net.chatfoodie.server.food.Food;
 import net.chatfoodie.server.food.dto.FoodRequest;
 import net.chatfoodie.server.food.dto.FoodResponse;
-import net.chatfoodie.server.food.repository.FoodRepository;
 import net.chatfoodie.server.food.service.FoodService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Collections;
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,15 +15,17 @@ public class FoodController {
     final private FoodService foodService;
 
     @GetMapping("foods/random")
-    public ResponseEntity<FoodResponse.FindAllDto> getRandomFoods(@RequestParam(value = "size",defaultValue = "30")
+    public ResponseEntity<?> getRandomFoods(@RequestParam(value = "size",defaultValue = "30")
                                                                   Integer size) {
         FoodResponse.FindAllDto responseDto = foodService.getRandomFoods(size);
-        return ResponseEntity.ok(responseDto);
+        ApiUtils.Response<?> response = ApiUtils.success(responseDto);
+        return ResponseEntity.ok().body(response);
     }
 
     @PostMapping("foods/saveUserFoodPreference")
-    public ResponseEntity<String> saveUserFoodPreference(@RequestBody FoodRequest.SaveUserFoodPreferenceDto requestDto) {
+    public ResponseEntity<?> saveUserFoodPreference(@RequestBody FoodRequest.SaveUserFoodPreferenceDto requestDto) {
         foodService.saveUserFoodPreference(requestDto);
-        return ResponseEntity.ok("ok");
+        ApiUtils.Response<?> response = ApiUtils.success();
+        return ResponseEntity.ok().body(response);
     }
 }
