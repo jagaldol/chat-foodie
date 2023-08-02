b5a8dcde9fc6ff5aabdeeeb6172c167526055749
icarus012832 <icarus012832@gmail.com>
2023-08-02 17:36:08 +0900

    refactor: Remove findById
    기본으로 지원하는 기능이라 중복되어 삭제함

--- server/src/main/java/net/chatfoodie/server/food/repository/FoodRepository.java
+++ server/src/main/java/net/chatfoodie/server/food/repository/FoodRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface FoodRepository extends JpaRepository<Food, Long> {
-    Optional<Food> findById(Long id);
 }
