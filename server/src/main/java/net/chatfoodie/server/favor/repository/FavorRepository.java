ff8b59088d2cb42414d631c57aeb809df9bee52f
Seongmin <74234978+sm136599@users.noreply.github.com>
2023-08-02 16:17:13 +0900

    refacor: Change function findByUser -> findByUserId

--- server/src/main/java/net/chatfoodie/server/favor/repository/FavorRepository.java
+++ server/src/main/java/net/chatfoodie/server/favor/repository/FavorRepository.java
@@ -1,12 +1,11 @@
 package net.chatfoodie.server.favor.repository;
 
 import net.chatfoodie.server.favor.Favor;
-import net.chatfoodie.server.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface FavorRepository extends JpaRepository<Favor, Long> {
 
-    Optional<Favor> findByUser(User user);
+    List<Favor> findByUserId(Long userId);
 }
