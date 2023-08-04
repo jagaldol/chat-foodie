6e05e1c7f646bbba8dd9723cd039e4adac641520
jagaldol <yoy06056@pusan.ac.kr>
2023-08-04 17:35:59 +0900

    feat: response By socketSession
    사용자별로 나뉘어서 리턴해줌

--- server/src/main/java/net/chatfoodie/server/message/handler/ChatHandler.java
+++ server/src/main/java/net/chatfoodie/server/message/handler/ChatHandler.java
@@ -9,34 +9,43 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 @Component
 public class ChatHandler extends TextWebSocketHandler {
 
-    private final static List<WebSocketSession> sessions = new ArrayList<>();
+    private final Map<WebSocketSession, String> userChatRooms = new ConcurrentHashMap<>();
 
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        sessions.add(session);
+        // 클라이언트가 채팅방에 입장 요청 시, 사용자와 채팅방을 매칭하여 저장
+        String userId = session.getId(); // 사용자 ID를 적절히 설정 (세션 ID를 사용할 수도 있음)
+        String chatRoomId = "chatRoom_" + userId; // 개별적인 채팅방 ID를 설정 (예: chatRoom_userId)
+        userChatRooms.put(session, chatRoomId);
         log.info(session + "클라이언트 접속");
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
-        log.info("payload : " + payload);
+        log.info("받은 메시지 : " + payload);
 
-        for(WebSocketSession s : sessions) {
-            TextMessage response = new TextMessage("Hello, " + payload + "!");
-            s.sendMessage(response);
+        String chatRoomId = userChatRooms.get(session);
+        for (WebSocketSession s : userChatRooms.keySet()) {
+            if (userChatRooms.get(s).equals(chatRoomId)) {
+                TextMessage textMessage = new TextMessage("사용자 " + session.getId() + " : " + payload);
+                s.sendMessage(textMessage);
+            }
         }
+        // TODO: 응답 이후 일정 시간 후 자동 Connection Close??
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         log.info(session + " 클라이언트 접속 해제");
-        sessions.remove(session);
+        userChatRooms.remove(session);
     }
 }
