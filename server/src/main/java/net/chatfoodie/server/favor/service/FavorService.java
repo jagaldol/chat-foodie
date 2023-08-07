bb6dadee25e230845d96b014587829ae734fb47f
icarus012832 <icarus012832@gmail.com>
2023-08-07 16:57:47 +0900

    fix: Edit get userId
    userId를 받아오는 방식을 JWT 적용하는 걸로 변경함

--- server/src/main/java/net/chatfoodie/server/favor/service/FavorService.java
+++ server/src/main/java/net/chatfoodie/server/favor/service/FavorService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import net.chatfoodie.server._core.errors.exception.Exception400;
 import net.chatfoodie.server._core.errors.exception.Exception404;
+import net.chatfoodie.server._core.security.CustomUserDetails;
 import net.chatfoodie.server.favor.Favor;
 import net.chatfoodie.server.favor.dto.FavorRequest;
 import net.chatfoodie.server.favor.repository.FavorRepository;
@@ -12,6 +13,7 @@ import net.chatfoodie.server.food.Food;
 import net.chatfoodie.server.food.repository.FoodRepository;
 import net.chatfoodie.server.user.User;
 import net.chatfoodie.server.user.repository.UserRepository;
+import net.chatfoodie.server.user.service.UserService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,13 +32,14 @@ public class FavorService {
     final private FoodRepository foodRepository;
     final private UserRepository userRepository;
 
-    @Transactional
-    public void saveUserFoodPreference(FavorRequest.SaveUserFoodPreferenceDto requestDto)  {
-        List<Long> foodIds = requestDto.foodIds();
-        Long userId = requestDto.userId();
-        User user = userRepository.findById(userId).orElseThrow(()-> new Exception404("해당 회원이 존재하지 않습니다"));
 
-        favorRepository.deleteAllByUserId(userId);
+
+    @Transactional
+    public void saveUserFoodPreference(Long id, FavorRequest.SaveUserFoodPreferenceDto requestDto)  {
+        List<Long> foodIds = requestDto.foodIds();
+        User user = userRepository.findById(id).orElseThrow(()-> new Exception404("해당 회원이 존재하지 않습니다"));
+
+        favorRepository.deleteAllByUserId(id);
 
         List<Food> foods = foodRepository.findByIdIn(foodIds);
         if (foodIds.size() != foods.size()) throw new Exception400("해당 음식이 존재하지 않습니다.");
@@ -49,7 +52,7 @@ public class FavorService {
                 .collect(Collectors.toList());
 
         favorRepository.saveAll(favors);
-                log.info("사용자의 선호 음식 정보가 저장되었습니다. 사용자 ID: {}, 음식 ID: {}", userId, foodIds);
+                log.info("사용자의 선호 음식 정보가 저장되었습니다. 사용자 ID: {}, 음식 ID: {}", id, foodIds);
             }
 
 }
