bf91547964de52b7f8b401f595bd449d9a4fc1e6
icarus012832 <icarus012832@gmail.com>
2023-08-09 15:53:28 +0900

    fix: Change Exception num
    exception 번호 변경

--- server/src/main/java/net/chatfoodie/server/chatroom/message/controller/MessageController.java
+++ server/src/main/java/net/chatfoodie/server/chatroom/message/controller/MessageController.java
@@ -9,8 +9,6 @@ import net.chatfoodie.server.chatroom.message.dto.MessageResponse;
 import net.chatfoodie.server.chatroom.message.service.MessageService;
 
 import org.springframework.http.ResponseEntity;
-
-
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
--- server/src/main/java/net/chatfoodie/server/chatroom/message/service/MessageService.java
+++ server/src/main/java/net/chatfoodie/server/chatroom/message/service/MessageService.java
@@ -2,7 +2,7 @@ package net.chatfoodie.server.chatroom.message.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.chatfoodie.server._core.errors.exception.Exception400;
+import net.chatfoodie.server._core.errors.exception.Exception403;
 import net.chatfoodie.server._core.errors.exception.Exception404;
 import net.chatfoodie.server.chatroom.Chatroom;
 import net.chatfoodie.server.chatroom.message.Message;
@@ -30,13 +30,13 @@ public class MessageService {
         Chatroom chatroom = chatroomRepository.findById(chatroomId)
                 .orElseThrow(() -> new Exception404("채팅방을 찾을 수 없습니다."));
 
-        // 채팅방의 대화내용을 조회하고, 최신 메시지 순서로 정렬합니다.
-        List<Message> chatHistory = messageRepository.findAllByChatroomIdOrderByIdAsc(chatroomId);
-
         // 채팅방의 유저와 현재 접속한 유저가 같은지 확인합니다.
         if (!Objects.equals(chatroom.getUser().getId(), userId)) {
-            throw new Exception400("현재 유저의 채팅방이 아닙니다.");
+            throw new Exception403("현재 유저의 채팅방이 아닙니다.");
         }
+
+        // 채팅방의 대화내용을 조회하고, 최신 메시지 순서로 정렬합니다.
+        List<Message> chatHistory = messageRepository.findAllByChatroomId(chatroomId);
 
 
         return MessageResponse.ContentList.of(chatHistory);
