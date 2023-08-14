696202c8a2914d6507ecb61784b5673f2f19bbbe
jagaldol <yoy06056@pusan.ac.kr>
2023-08-14 15:04:11 +0900

    fix: Button lint error

--- client/src/components/modal.tsx
+++ client/src/components/modal.tsx
@@ -4,7 +4,7 @@ import titleFont from "@/styles/TitleFont"
 
 export default function Modal({ content, onClickClose }: { content: ReactNode; onClickClose: MouseEventHandler }) {
   return (
-    <div className="bg-black/10 h-screen w-screen z-30 flex fixed left-0 top-0 items-center justify-center">
+    <div className="bg-black/5 h-screen w-screen z-30 flex fixed left-0 top-0 items-center justify-center">
       <div className="w-[500px] h-[650px] bg-white drop-shadow p-5">
         <div className="flex justify-end">
           <Image
@@ -17,11 +17,11 @@ export default function Modal({ content, onClickClose }: { content: ReactNode; o
           />
         </div>
         <div className="flex flex-col justify-center">
-          <div className="flex items-center justify-center">
+          <div className="flex items-center justify-center mr-5">
             <Image src="/svg/logo.svg" alt="logo" width={36} height={36} />
             <h1 className={`text-3xl text-main-theme font-bold ${titleFont.className}`}>Chatfoodie</h1>
           </div>
-          <p className="text-sm text-center ml-5">음식 추천의 전문가 foodie와 채팅을 해보세요!</p>
+          <p className="text-sm text-center">음식 추천의 전문가 foodie와 채팅을 해보세요!</p>
         </div>
         {content}
       </div>
--- client/src/containers/login/account.tsx
+++ client/src/containers/login/account.tsx
@@ -8,14 +8,19 @@ export default function Account() {
 
   return (
     <div className="flex flex-wrap grow justify-end items-center min-w-[20%] h-min">
-      <div className="flex items-center justify-center mr-9 my-1 bg-main-theme text-white rounded hover:cursor-pointer">
-        <p className="text-sm mx-2.5 my-2 text-center" onClick={() => setLoginModalOpened(true)}>
-          로그인
-        </p>
-      </div>
-      <div className="flex items-center justify-center mr-9 my-1 bg-main-theme text-white rounded hover:cursor-pointer">
+      <button
+        type="button"
+        className="flex items-center justify-center mr-9 my-1 bg-main-theme text-white rounded hover:cursor-pointer"
+        onClick={() => setLoginModalOpened(true)}
+      >
+        <p className="text-sm mx-2.5 my-2 text-center">로그인</p>
+      </button>
+      <button
+        type="button"
+        className="flex items-center justify-center mr-9 my-1 bg-main-theme text-white rounded hover:cursor-pointer"
+      >
         <p className="text-sm mx-2.5 my-2 text-center">회원가입</p>
-      </div>
+      </button>
       {loginModalOpened ? <LoginModal onClickClose={() => setLoginModalOpened(false)} /> : null}
     </div>
   )
