46ea9225e547990355ff9f41de40ff603e5acbac
jagaldol <yoy06056@pusan.ac.kr>
2023-08-15 03:37:08 +0900

    feat: Modal 구조 개선 children 사용

--- client/src/containers/account/loginModal.tsx
+++ client/src/containers/account/loginModal.tsx
@@ -11,51 +11,51 @@ export default function LoginModal({
   onClickClose(): void
   onChangeAccount(): void
 }) {
-  const content = (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault()
-        const idInput = e.currentTarget.querySelector("[name='id']") as HTMLInputElement
-        const id = idInput.value
-        const passwordInput = e.currentTarget.querySelector("[name='password']") as HTMLInputElement
-        const password = passwordInput.value
-        proxy
-          .post("/login", {
-            loginId: id,
-            password,
-          })
-          .then((res) => {
-            const jwt = res.headers.authorization
-            saveJwt(jwt)
-            alert("로그인 성공")
-            onChangeAccount()
-            onClickClose()
-          })
-          .catch((res) => {
-            alert(res.response.data.errorMessage)
-          })
-      }}
-    >
-      <label className="flex items-center" htmlFor="id">
-        <p className="mr-9 w-12">ID</p>
-        <input name="id" className="grow border border-solid border-black rounded my-3" />
-      </label>
-      <label className="flex items-center" htmlFor="password">
-        <p className="mr-9 w-12">Password</p>
-        <input name="password" className="grow border border-solid border-black rounded my-3" />
-      </label>
-      <div className="flex justify-center m">
-        <button
-          className="w-full bg-main-theme text-white px-3 py-1 rounded"
-          type="submit"
-          onClick={() => {}}
-          value="버튼"
-        >
-          로그인
-        </button>
-      </div>
-    </form>
+  return (
+    <Modal onClickClose={onClickClose}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          const idInput = e.currentTarget.querySelector("[name='id']") as HTMLInputElement
+          const id = idInput.value
+          const passwordInput = e.currentTarget.querySelector("[name='password']") as HTMLInputElement
+          const password = passwordInput.value
+          proxy
+            .post("/login", {
+              loginId: id,
+              password,
+            })
+            .then((res) => {
+              const jwt = res.headers.authorization
+              saveJwt(jwt)
+              alert("로그인 성공")
+              onChangeAccount()
+              onClickClose()
+            })
+            .catch((res) => {
+              alert(res.response.data.errorMessage)
+            })
+        }}
+      >
+        <label className="flex items-center" htmlFor="id">
+          <p className="mr-9 w-12">ID</p>
+          <input name="id" className="grow border border-solid border-black rounded my-3" />
+        </label>
+        <label className="flex items-center" htmlFor="password">
+          <p className="mr-9 w-12">Password</p>
+          <input name="password" className="grow border border-solid border-black rounded my-3" />
+        </label>
+        <div className="flex justify-center m">
+          <button
+            className="w-full bg-main-theme text-white px-3 py-1 rounded"
+            type="submit"
+            onClick={() => {}}
+            value="버튼"
+          >
+            로그인
+          </button>
+        </div>
+      </form>
+    </Modal>
   )
-
-  return <Modal content={content} onClickClose={onClickClose} />
 }
