8378f949203f9534068655b80aaa95abb23f8a42
jagaldol <yoy06056@pusan.ac.kr>
2023-08-16 22:34:30 +0900

    feat: Send Message when Press Enter

--- client/src/containers/chat/message-input.tsx
+++ client/src/containers/chat/message-input.tsx
@@ -3,6 +3,28 @@ import { limitInputNumber, pressEnter } from "@/utils/utils"
 import { ChatMessage } from "@/types/chat"
 
 export default function MessageInput({ addMessage }: { addMessage: (message: ChatMessage) => void }) {
+  const onSendClick = () => {
+    const userInputBox = document.getElementById("user-input-box") as HTMLTextAreaElement
+
+    if (userInputBox.value) {
+      const userMessage: ChatMessage = {
+        id: 0,
+        content: userInputBox.value,
+        isFromChatbot: false,
+      }
+      addMessage(userMessage)
+      // 서버 요청 하기??
+      const chatbotMessage: ChatMessage = {
+        id: 0,
+        content: userInputBox.value,
+        isFromChatbot: true,
+      }
+      addMessage(chatbotMessage)
+
+      userInputBox.value = ""
+    }
+  }
+
   return (
     <div className="flex justify-center mb-6 w-[60%] border-2 border-solid border-gray-400 rounded py-3 box-content">
       <textarea
@@ -13,29 +35,12 @@ export default function MessageInput({ addMessage }: { addMessage: (message: Cha
           e.target.style.height = `${Math.min(e.target.scrollHeight, 120)}px`
           limitInputNumber(e, 500)
         }}
-        onKeyDown={(e) => pressEnter(e, () => {})}
+        onKeyDown={(e) => {
+          pressEnter(e, onSendClick)
+        }}
         placeholder="메시지를 입력해주세요"
       />
-      <button
-        type="button"
-        className="w-10 flex justify-center items-center"
-        onClick={() => {
-          const userInputBox = document.getElementById("user-input-box") as HTMLTextAreaElement
-          const userMessage: ChatMessage = {
-            id: 0,
-            content: userInputBox.value,
-            isFromChatbot: false,
-          }
-          addMessage(userMessage)
-          // 서버 요청 하기??
-          const chatbotMessage: ChatMessage = {
-            id: 0,
-            content: userInputBox.value,
-            isFromChatbot: true,
-          }
-          addMessage(chatbotMessage)
-        }}
-      >
+      <button type="button" className="w-10 flex justify-center items-center" onClick={onSendClick}>
         <Image src="/svg/send.svg" alt="전송" width="16" height="16" />
       </button>
     </div>
