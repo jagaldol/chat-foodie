0513b1c1dba2db66ce1a51c42447e0f99b68bed9
jagaldol <yoy06056@pusan.ac.kr>
2023-08-16 21:11:22 +0900

    refactor: 구조 개선

--- client/src/containers/account/preferenceModal.tsx
+++ client/src/containers/account/preferenceModal.tsx
@@ -13,6 +13,39 @@ export default function PreferenceModal({ onClickClose }: { onClickClose(): void
 
   const foodsPerPage = 6
 
+  const handleFoodClick = (foodId: number) => {
+    if (selectedFoods.includes(foodId)) {
+      setSelectedFoods(selectedFoods.filter((id) => id !== foodId))
+    } else {
+      setSelectedFoods([...selectedFoods, foodId])
+    }
+  }
+
+  const handleSendSelection = () => {
+    // 선택한 음식의 리스트를 서버로 전송하는 로직 추가
+    if (selectedFoods.length === 0) {
+      alert("하나 이상의 음식을 선택해 주세요")
+      return
+    }
+    const headers = {
+      Authorization: getJwtTokenFromStorage(),
+    }
+    const requestData = {
+      foodIds: selectedFoods,
+    }
+
+    // 서버로 선택한 음식 ID 리스트를 전송하는 로직 추가
+    proxy
+      .post("/favors", requestData, { headers })
+      .then(() => {
+        alert("선호하는 음식이 저장되었습니다!")
+        onClickClose()
+      })
+      .catch(() => {
+        alert("오류가 발생하였습니다.")
+      })
+  }
+
   useEffect(() => {
     setLoading(true)
     proxy
@@ -28,90 +61,53 @@ export default function PreferenceModal({ onClickClose }: { onClickClose(): void
       })
   }, [])
 
-  const handleFoodClick = (foodId: number) => {
-    if (selectedFoods.includes(foodId)) {
-      setSelectedFoods(selectedFoods.filter((id) => id !== foodId))
-    } else {
-      setSelectedFoods([...selectedFoods, foodId])
-    }
-  }
-
-  const handlePageChange = (newPage: number) => {
-    setCurrentPage(newPage)
-  }
-
-  const renderFoods = () => {
-    if (!foodList) {
-      return null
-    }
-
-    const startIndex = (currentPage - 1) * foodsPerPage
-    const foodsOnPage = foodList.slice(startIndex, startIndex + foodsPerPage)
-
-    return foodsOnPage.map((food) => (
-      <div key={food.id} className={`food ${selectedFoods.includes(food.id) ? "selected-food" : "unselected-food"}`}>
-        <button
-          type="button"
-          onClick={() => handleFoodClick(food.id)}
-          className={`food-button ${selectedFoods.includes(food.id) ? "selected" : ""}`}
-        >
-          <div
-            className={`food-image ${
-              selectedFoods.includes(food.id) ? "selected-image ring-2 ring-orange-500" : "unselected-image"
-            } m-2 rounded-md`}
-          >
-            <Image src={`${process.env.NEXT_PUBLIC_API_URL}${food.imageUrl}`} alt={food.name} width={140} height={95} />
-          </div>
-          <p>{food.name}</p>
-        </button>
-      </div>
-    ))
-  }
-
-  const handleSendSelection = () => {
-    // 선택한 음식의 리스트를 서버로 전송하는 로직 추가
-    if (selectedFoods.length === 0) {
-      alert("하나 이상의 음식을 선택해 주세요")
-      return
-    }
-
-    const jwtToken = getJwtTokenFromStorage()
-
-    const headers = {
-      Authorization: jwtToken,
-    }
-
-    const requestData = {
-      foodIds: selectedFoods,
-    }
-
-    // 서버로 선택한 음식 ID 리스트를 전송하는 로직 추가
-    proxy
-      .post("/favors", requestData, {
-        headers, // headers 객체를 직접 넣어줘야 합니다.
-      })
-      .then(() => {
-        alert("선호하는 음식이 저장되었습니다!")
-        onClickClose()
-      })
-      .catch(() => {
-        alert("오류가 발생하였습니다.")
-      })
-  }
-
   return (
     <Modal onClickClose={onClickClose} description="선호 하는 음식들을 선택해주세요.">
       {loading ? (
         <p>Loading...</p>
       ) : (
         <div className="flex flex-col items-center">
-          <div className="flex flex-wrap justify-center foods-container">{renderFoods()}</div>
+          <div className="flex flex-wrap justify-center">
+            {foodList
+              .filter((_, index) => {
+                const startIndex = (currentPage - 1) * foodsPerPage
+                return startIndex <= index && index < startIndex + foodsPerPage
+              })
+              .map((food) => {
+                return (
+                  <div
+                    key={food.id}
+                    className={`food ${selectedFoods.includes(food.id) ? "selected-food" : "unselected-food"}`}
+                  >
+                    <button
+                      type="button"
+                      onClick={() => handleFoodClick(food.id)}
+                      className={`food-button ${selectedFoods.includes(food.id) ? "selected" : ""}`}
+                    >
+                      <div
+                        className={`food-image ${
+                          selectedFoods.includes(food.id) ? "selected-image ring-2 ring-orange-500" : "unselected-image"
+                        } m-2 rounded-md`}
+                      >
+                        <Image
+                          src={`${process.env.NEXT_PUBLIC_API_URL}${food.imageUrl}`}
+                          alt={food.name}
+                          width={140}
+                          height={95}
+                        />
+                      </div>
+                      <p>{food.name}</p>
+                    </button>
+                  </div>
+                )
+              })}
+          </div>
           <div className="text-center mt-3">Page {currentPage} / 5</div>
           <div className="flex justify-center mt-3 space-x-2">
             {currentPage > 1 && (
               <button
                 type="button"
-                onClick={() => handlePageChange(currentPage - 1)}
+                onClick={() => setCurrentPage(currentPage - 1)}
                 className="px-4 py-2 bg-blue-500 text-white rounded-md"
               >
                 이전
@@ -120,7 +116,7 @@ export default function PreferenceModal({ onClickClose }: { onClickClose(): void
             {currentPage < 5 && (
               <button
                 type="button"
-                onClick={() => handlePageChange(currentPage + 1)}
+                onClick={() => setCurrentPage(currentPage + 1)}
                 className="px-4 py-2 bg-blue-500 text-white rounded-md"
               >
                 다음
