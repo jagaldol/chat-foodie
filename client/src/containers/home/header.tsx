dc2b0cca32fcb767be47d2441dcc4bdeeceab5d7
박성민 <sm136599@gmail.com>
2023-08-28 16:54:55 +0900

    feat: Change font size when less than md(768px)

--- client/src/containers/home/header.tsx
+++ client/src/containers/home/header.tsx
@@ -11,9 +11,13 @@ export default function Header() {
           <div className="mt-1 min-w-fit">
             <Image src="/svg/logo.svg" alt="logo" width={36} height={36} />
           </div>
-          <h1 className={`text-5xl text-main-theme font-bold ml-1 ${titleFont.className}`}>Chatfoodie</h1>
+          <h1 className={`text-5xl max-md:text-4xl text-main-theme font-bold ml-1 ${titleFont.className}`}>
+            Chatfoodie
+          </h1>
         </a>
-        <p className="text-sm text-center whitespace-nowrap">음식 추천의 전문가 foodie와 채팅을 해보세요!</p>
+        <p className="text-sm max-md:text-xs text-center whitespace-nowrap">
+          음식 추천의 전문가 foodie와 채팅을 해보세요!
+        </p>
       </div>
       <div className="basis-1/3">
         <Account />
