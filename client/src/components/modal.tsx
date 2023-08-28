8b016e52e363a35e1f37382fb295337eb15ec6c5
jagaldol <yoy06056@pusan.ac.kr>
2023-08-28 21:23:32 +0900

    feat: Move modal by drag modal header

--- client/src/components/modal.tsx
+++ client/src/components/modal.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react"
+import React, { ReactNode, useRef } from "react"
 import Image from "next/image"
 import titleFont from "@/styles/TitleFont"
 
@@ -11,10 +11,46 @@ export default function Modal({
   onClickClose(): void
   description?: string
 }) {
+  const modal = useRef<HTMLDivElement>(null)
+
+  /**
+   * 모달의 헤더를 클릭하여 모달을 움직일 수 있게 해주는 함수
+   * @param {MouseDownEvent<HTMLDivElement>} e
+   */
+  function moveModal(e: React.MouseEvent<HTMLDivElement>) {
+    if (e.target !== e.currentTarget) return
+    let startX = e.clientX
+    let startY = e.clientY
+
+    const onMove = (mouseEvent: MouseEvent) => {
+      const lastX = startX - mouseEvent.clientX
+      const lastY = startY - mouseEvent.clientY
+
+      startX = mouseEvent.clientX
+      startY = mouseEvent.clientY
+
+      if (modal.current) {
+        modal.current.style.top = `${modal.current.offsetTop - lastY}px`
+        modal.current.style.left = `${modal.current.offsetLeft - lastX}px`
+      }
+    }
+
+    const onRemoveEvent = () => {
+      document.removeEventListener("mouseup", onRemoveEvent)
+      document.removeEventListener("mousemove", onMove)
+    }
+
+    document.addEventListener("mousemove", onMove)
+    document.addEventListener("mouseup", onRemoveEvent)
+  }
+
   return (
     <div className="bg-black/5 h-full w-full z-30 flex fixed left-0 top-0 items-center justify-center">
-      <div className="flex flex-col w-[500px] h-[650px] bg-white drop-shadow p-5 max-md:w-[348px] max-md:h-[540px] max-md:p-3">
-        <div className="flex justify-end">
+      <div
+        ref={modal}
+        className="flex fixed flex-col w-[500px] h-[650px] bg-white shadow-[3px_3px_11px_0_rgba(0,0,0,0.1)] p-5 max-md:w-[348px] max-md:h-[540px] max-md:p-3"
+      >
+        <div role="presentation" className="flex justify-end" onMouseDown={(e) => moveModal(e)}>
           <Image
             src="/svg/close.svg"
             alt="close"
