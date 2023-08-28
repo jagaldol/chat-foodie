752ddbc3ef8523588d69e302dc4810374c1399a4
jagaldol <yoy06056@pusan.ac.kr>
2023-08-29 04:01:31 +0900

    feat: Add Favicons

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css"
 import type { Metadata } from "next"
 import { Noto_Sans_KR } from "next/font/google"
 import { ReactNode } from "react"
+import Head from "next/head"
 import Header from "@/containers/home/header"
 import Footer from "@/containers/home/footer"
 import Navigator from "@/containers/home/navigator"
@@ -15,14 +16,21 @@ const notoSans = Noto_Sans_KR({
   fallback: ["Malgun Gothic", "Roboto", "sans-serif"],
 })
 
-export const metadata: Metadata = {
-  title: "Chat Foodie",
-  description: "음식 추천의 전문가 푸디와 채팅을 해보세요!",
-}
-
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
+        <title>Chat Foodie</title>
+        <meta name="description" content="음식 추천의 전문가 푸디와 채팅을 해보세요!" />
+      </head>
       <AuthProvider>
         <ChatroomProvider>
           <body
