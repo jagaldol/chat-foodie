55acfc5fdcc0766961a2ae3c4691d254bd40f7b1
jagaldol <yoy06056@pusan.ac.kr>
2023-08-29 18:50:32 +0900

    fix: resolve eslint

--- client/src/app/chat/layout.tsx
+++ client/src/app/chat/layout.tsx
@@ -1,18 +1,10 @@
 import "@/styles/globals.css"
-import { Noto_Sans_KR } from "next/font/google"
 import { ReactNode } from "react"
 import Header from "@/containers/home/header"
 import Footer from "@/containers/home/footer"
 import Navigator from "@/containers/home/navigator"
 import AuthProvider from "@/contexts/authContextProvider"
 import ChatroomProvider from "@/contexts/chatroomContextProvider"
-
-const notoSans = Noto_Sans_KR({
-  weight: ["100", "300", "400", "500", "700", "900"],
-  subsets: ["latin"],
-  display: "swap",
-  fallback: ["Malgun Gothic", "Roboto", "sans-serif"],
-})
 
 export default function ChatLayout({ children }: { children: ReactNode }) {
   return (
