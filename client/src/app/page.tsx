151669eca9149c19ae7d940296495af1f6b234df
jagaldol <yoy06056@pusan.ac.kr>
2023-09-05 13:25:55 +0900

    fix: 디자인 시안에 맞게 css 수정

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -5,8 +5,8 @@ import titleFont from "@/styles/TitleFont"
 function ParagraphBox({ children, title }: { children: ReactNode; title: string }) {
   return (
     <div className="pt-32">
-      <h2 className="text-2xl font-semibold">{title}</h2>
-      <div className="text-lg mt-2">{children}</div>
+      <h2 className="text-3xl font-semibold">{title}</h2>
+      <div className="text-lg leading-10 mt-10">{children}</div>
     </div>
   )
 }
@@ -30,24 +30,27 @@ export default function Home() {
           </a>
         </div>
       </div>
-      <div className="leading-loose my-20 mx-56">
+      <div className="my-20 mx-56">
         <ParagraphBox title="Chatfoodie는...">
           <p>
             LLM(Large Language Model)을 파인튜닝한 <b>음식 추천을 위한 챗봇 서비스</b>입니다.
           </p>
           <br />
-          <p>최근 거대 언어 모델(LLM)의 발전으로 인해 chatGPT와 같은 대화형 챗봇이 많이 개발되고 있습니다.</p>
-          <p>이러한 대화형 챗봇은 사용자와의 대화를 통해 사용자의 요구에 맞는 맞춤형 대답을 생성하게 됩니다.</p>
           <p>
-            저희 팀은 LLM의 이런 특성에 착안하여 대화를 통해 편리하게 원하는 음식을 추천 받을 수 있는 음식 추천 챗봇을
-            개발하였습니다.
+            최근 거대 언어 모델(LLM)의 발전으로 인해 chatGPT와 같은 대화형 챗봇이 많이 개발되고 있습니다.이러한 대화형
+            챗봇은 사용자와의 대화를 통해 사용자의 요구에 맞는 맞춤형 대답을 생성하게 됩니다. 저희 팀은 LLM의 이런
+            특성에 착안하여 대화를 통해 편리하게 원하는 음식을 추천 받을 수 있는 음식 추천 챗봇을 개발하였습니다.
           </p>
         </ParagraphBox>
         <ParagraphBox title="모델 학습">
           <div className="flex">
             <div className="mr-5">
-              <p>베이스 모델로 KoAlpaca-polyglot-5.8B를 사용하였습니다.</p>
-              <p>QLoRA 기법을 이용하여 Colab 무료 버전으로 학습을 진행하였습니다.</p>
+              <p>
+                베이스 모델로 <b>KoAlpaca-polyglot-5.8B</b>를 사용하였습니다.
+              </p>
+              <p>
+                <b>QLoRA 기법</b>을 이용하여 Colab 무료 버전으로 학습을 진행하였습니다.
+              </p>
             </div>
             <Image src="/svg/trainingLoss.svg" alt="loss" width={400} height={316} />
           </div>
@@ -61,7 +64,7 @@ export default function Home() {
           </ul>
         </ParagraphBox>
       </div>
-      <footer className="flex flex-col justify-center items-center text-xs text-[#666666] mt-10 mb-1">
+      <footer className="flex flex-col justify-center items-center text-xs text-[#444654] mt-10 mb-5">
         <p>
           &copy; Chatfoodie. All right reserved. food recommendation chatbot with LLM.food recommendation chatbot with
           LLM.
