9a1deb62147bb52141a1a5036335c50af0eb5fde
jagaldol <yoy06056@pusan.ac.kr>
2023-09-05 13:15:17 +0900

    feat: ParagraphBox 컴포넌트화 및 markup 구조 개선

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,15 @@
-import React from "react"
+import React, { ReactNode } from "react"
 import Image from "next/image"
 import titleFont from "@/styles/TitleFont"
+
+function ParagraphBox({ children, title }: { children: ReactNode; title: string }) {
+  return (
+    <div className="pt-32">
+      <h2 className="text-2xl font-semibold">{title}</h2>
+      <div className="text-lg mt-2">{children}</div>
+    </div>
+  )
+}
 
 export default function Home() {
   return (
@@ -21,42 +30,36 @@ export default function Home() {
           </a>
         </div>
       </div>
-      <div className="leading-loose">
-        <div className="my-20 mx-56">
-          <div className="pt-32">
-            <h2 className="text-2xl font-semibold">Chatfoodie는...</h2>
-            <p className="text-lg mt-2">
-              LLM(Large Language Model)을 파인튜닝한 <b>음식 추천을 위한 챗봇 서비스</b>입니다.
-              <br />
-              <br /> 최근 거대 언어 모델(LLM)의 발전으로 인해 chatGPT와 같은 대화형 챗봇이 많이 개발되고 있습니다.{" "}
-              <br />
-              이러한 대화형 챗봇은 사용자와의 대화를 통해 사용자의 요구에 맞는 맞춤형 대답을 생성하게 됩니다. <br />
-              저희 팀은 LLM의 이런 특성에 착안하여 대화를 통해 편리하게 원하는 음식을 추천 받을 수 있는 음식 추천 챗봇을
-              개발하였습니다.
-            </p>
-          </div>
-          <div className="pt-32">
-            <h2 className="text-2xl font-semibold">모델 학습</h2>
-            <div style={{ display: "flex" }}>
-              <p style={{ marginRight: "20px" }}>
-                베이스 모델로 KoAlpaca-polyglot-5.8B를 사용하였습니다.
-                <br /> QLoRA 기법을 이용하여 Colab 무료 버전으로 학습을
-                <br />
-                진행하였습니다.
-              </p>
-              <Image src="/svg/trainingLoss.svg" alt="loss" width={400} height={316} />
+      <div className="leading-loose my-20 mx-56">
+        <ParagraphBox title="Chatfoodie는...">
+          <p>
+            LLM(Large Language Model)을 파인튜닝한 <b>음식 추천을 위한 챗봇 서비스</b>입니다.
+          </p>
+          <br />
+          <p>최근 거대 언어 모델(LLM)의 발전으로 인해 chatGPT와 같은 대화형 챗봇이 많이 개발되고 있습니다.</p>
+          <p>이러한 대화형 챗봇은 사용자와의 대화를 통해 사용자의 요구에 맞는 맞춤형 대답을 생성하게 됩니다.</p>
+          <p>
+            저희 팀은 LLM의 이런 특성에 착안하여 대화를 통해 편리하게 원하는 음식을 추천 받을 수 있는 음식 추천 챗봇을
+            개발하였습니다.
+          </p>
+        </ParagraphBox>
+        <ParagraphBox title="모델 학습">
+          <div className="flex">
+            <div className="mr-5">
+              <p>베이스 모델로 KoAlpaca-polyglot-5.8B를 사용하였습니다.</p>
+              <p>QLoRA 기법을 이용하여 Colab 무료 버전으로 학습을 진행하였습니다.</p>
             </div>
+            <Image src="/svg/trainingLoss.svg" alt="loss" width={400} height={316} />
           </div>
-          <div className="pt-32">
-            <h2 className="text-2xl font-semibold">개발 팀</h2>
-            <p>2023년 부산대학교 정보컴퓨터공학부 졸업과제 - 팀 쩝쩝학사</p>
-            <ul className="text-lg mt-2 list-disc list-inside">
-              <li>안혜준(팀장)</li>
-              <li>박성민</li>
-              <li>박진영</li>
-            </ul>
-          </div>
-        </div>
+        </ParagraphBox>
+        <ParagraphBox title="개발 팀">
+          <p>2023년 부산대학교 정보컴퓨터공학부 졸업과제 - 팀 쩝쩝학사</p>
+          <ul className="mt-2 list-disc list-inside">
+            <li>안혜준(팀장)</li>
+            <li>박성민</li>
+            <li>박진영</li>
+          </ul>
+        </ParagraphBox>
       </div>
       <footer className="flex flex-col justify-center items-center text-xs text-[#666666] mt-10 mb-1">
         <p>
