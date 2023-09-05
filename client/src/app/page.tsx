1fe7f0b7d36427723e11032a563db98ab12cbce7
jagaldol <yoy06056@pusan.ac.kr>
2023-09-06 04:24:31 +0900

    feat: 내용 추가 및 최종 완료

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
           <br />
           <div className="flex max-sm:flex-col">
             <div className="mr-5">
-              <p>
+              <p className="indent-4">
                 Polyglot-ko는 한국어 데이터로 학습된 다국어 초거대 언어모델 개발 프로젝트로 한국어 성능이 타 LLM보다
                 우수합니다. KoAlpaca-polyglot-5.8B는 한국어에 강점을 보이는 polyglot-ko를 KoAlpaca에서 챗봇 용도로
                 학습시켜 놓은 모델입니다.
@@ -73,26 +73,109 @@ export default function Home() {
           </div>
           <br />
           <div className="flex max-sm:flex-col-reverse">
-            <div className="flex shrink-0 justify-center max-sm:mt-10 mr-5">
+            <div className="flex shrink-0 justify-center max-sm:mt-10 mr-5 max-sm:mr-0">
               <ImageBox src="/assets/train-loss-5epoch.png" caption="학습률 - traing loss" />
             </div>
             <div>
-              <p>
+              <p className="indent-4">
                 SNS 대화 데이터(AI hub)와 Wikipedia의 음식 정보를 바탕으로 GPT를 사용한 <b>self-instruct</b>를
-                수행하였고 최종 8000개의 학습 데이터를 이용하였습니다. 학습의 경우, <b>QLoRA 기법</b>을 이용하여 Colab
+                수행하였고 최종 8,000개의 학습 데이터를 이용하였습니다. 학습의 경우, <b>QLoRA 기법</b>을 이용하여 Colab
                 무료 버전으로 학습을 진행할 수 있습니다.
               </p>
             </div>
           </div>
         </ParagraphBox>
+        <ParagraphBox title="시스템 구조">
+          <figure className="flex flex-col items-center">
+            <div className="relative w-[600px] h-[450px] max-sm:w-64 max-sm:h-48 bg-gray-50 rounded-3xl">
+              <Image src="/assets/system-blueprint.png" alt="loss" fill className="object-scale-down p-5 max-sm:p-1" />
+            </div>
+            <figcaption className="text-center text-sm leading-8 max-sm:text-xs max-sm:leading-7">
+              전체 챗봇 채팅 시스템 구조
+            </figcaption>
+          </figure>
+        </ParagraphBox>
+        <ParagraphBox title="코드 및 모델 확인하기">
+          <ul className="mt-2 pl-5 leading-none">
+            <li className="my-5">
+              <a
+                href="https://github.com/jagaldol/chat-foodie"
+                className="flex items-baseline gap-1 w-fit hover:underline"
+              >
+                <p>- 서비스 전체 코드</p>
+                <Image src="/svg/github.svg" alt="" height="16" width="16" style={{ height: "16px" }} />
+              </a>
+            </li>
+            <li className="my-5">
+              <a
+                href="https://github.com/jagaldol/chat-foodie-chatbot-server"
+                className="flex items-baseline gap-1 w-fit h-fit hover:underline"
+              >
+                <p>- AI 챗봇 웹소켓 API 서버</p>
+                <Image src="/svg/github.svg" alt="" height="16" width="16" style={{ height: "16px" }} />
+              </a>
+            </li>
+            <li className="my-5">
+              <a
+                href="https://huggingface.co/datasets/jagaldol/chat-foodie"
+                className="flex items-baseline gap-1 hover:underline"
+              >
+                <p>- 챗봇 학습 데이터 세트 8,000개</p>
+                <Image src="/svg/hf-logo.svg" alt="" height="16" width="16" style={{ height: "16px" }} />
+              </a>
+            </li>
+            <li className="my-5">
+              <a
+                href="https://huggingface.co/sm136599/chatfoodie-koalpaca-polyglot-5_8b-5150step-8batch_5epoch"
+                className="flex items-baseline gap-1 hover:underline"
+              >
+                <p>- 챗봇 LoRA 모델</p>
+                <Image src="/svg/hf-logo.svg" alt="" height="16" width="16" style={{ height: "16px" }} />
+              </a>
+            </li>
+            <li className="my-5">
+              <a
+                href="https://colab.research.google.com/github/jagaldol/chat-foodie/blob/dev/chatbot/Deploy_chatbot_server_as_public_with_colab.ipynb"
+                className="flex items-baseline gap-1 hover:underline"
+              >
+                <p>- 푸디 AI QLoRA 파인튜닝 예제</p>
+                <Image src="/svg/colab.svg" alt="" height="16" width="16" style={{ height: "16px" }} />
+              </a>
+            </li>
+            <li className="my-5">
+              <a
+                href="https://colab.research.google.com/github/jagaldol/chat-foodie/blob/dev/chatbot/Deploy_chatbot_server_as_public_with_colab.ipynb"
+                className="flex items-baseline gap-1 hover:underline"
+              >
+                <p>- 코랩을 사용한 챗봇 websocket api 배포 예제</p>
+                <Image src="/svg/colab.svg" alt="" height="16" width="16" style={{ height: "16px" }} />
+              </a>
+            </li>
+          </ul>
+        </ParagraphBox>
         <ParagraphBox title="개발 팀">
           <p>
             2023년 부산대학교 정보컴퓨터공학부 졸업과제 - <b className="whitespace-nowrap">팀 쩝쩝학사</b>
           </p>
-          <ul className="mt-2 list-disc list-inside">
-            <li>🚩안혜준(팀장)</li>
-            <li>💻박성민</li>
-            <li>💻박진영</li>
+          <ul className="mt-2 pl-5">
+            <li>
+              🚩
+              <a href="https://github.com/jagaldol" className="hover:underline">
+                안혜준(팀장)
+              </a>
+            </li>
+            <li>
+              💻
+              <a href="https://github.com/sm136599" className="hover:underline">
+                박성민
+              </a>
+            </li>
+            <li>
+              💻
+              <a href="https://github.com/icarus012832" className="hover:underline">
+                박진영
+              </a>
+            </li>
           </ul>
         </ParagraphBox>
       </div>
