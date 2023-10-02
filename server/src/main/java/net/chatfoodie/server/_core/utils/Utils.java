c17b79aaa4c3fac001fb0de32676c12fe7744853
박성민 <sm136599@gmail.com>
2023-10-02 23:11:58 +0900

    refactor: extract method sendEmail to Utils
    여러 곳에서 쓰이는 이메일 전송 함수를 Utils 클래스에 정의했습니다.

--- server/src/main/java/net/chatfoodie/server/_core/utils/Utils.java
+++ server/src/main/java/net/chatfoodie/server/_core/utils/Utils.java
@@ -1,15 +1,19 @@
 package net.chatfoodie.server._core.utils;
 
+import jakarta.mail.internet.MimeMessage;
 import lombok.extern.slf4j.Slf4j;
+import net.chatfoodie.server._core.config.MailConfig;
 import net.chatfoodie.server._core.errors.exception.Exception500;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.JavaMailSender;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.time.format.FormatStyle;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 @Slf4j
 public class Utils {
@@ -44,5 +48,40 @@ public class Utils {
             return day <= dayOfLeapYear;
         }
         return day <= dayByMonth.get(month - 1);
+    }
+
+    public static String generateRandomPassword() {
+        Random random = new Random();
+
+        String PASSWORD_ALLOW_BASE = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&";
+        Integer PASSWORD_LENGTH = 16;
+
+        String password = "";
+
+        for (int i = 0; i < PASSWORD_LENGTH; i++) {
+            int rndCharAt = random.nextInt(PASSWORD_ALLOW_BASE.length());
+            char rndChar = PASSWORD_ALLOW_BASE.charAt(rndCharAt);
+            password += rndChar;
+        }
+
+        return password;
+    }
+
+    public static void sendEmail(JavaMailSender javaMailSender, String to, String subject, String text) {
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "utf-8");
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text, true);
+            javaMailSender.send(mimeMessage);
+        } catch (Exception e) {
+            StackTraceElement[] err = e.getStackTrace();
+            for (StackTraceElement stackTraceElement: err
+                 ) {
+                log.error(stackTraceElement.toString());
+            }
+            throw new Exception500("서버 이메일 전송 한도가 초과되었습니다. 내일 다시 시도해주세요.");
+        }
     }
 }
