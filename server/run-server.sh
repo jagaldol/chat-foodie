15a34ddd143290d3ccc9b9203e9342c3e1c3169c
jagaldol <yoy06056@pusan.ac.kr>
2023-10-07 16:19:31 +0900

    build: version up to v1.0.0

--- server/run-server.sh
+++ server/run-server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar -Dspring.profiles.active=product,mysql,private build/libs/server-0.0.1.jar
+java -jar -Dspring.profiles.active=product,mysql,private build/libs/server-1.0.0.jar
