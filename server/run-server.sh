f53ba12d8de2ca765ec94189c9ef7fa81ce99dbc
jagaldol <yoy06056@pusan.ac.kr>
2023-12-23 20:56:38 +0900

    fix: Update active profiles

--- server/run-server.sh
+++ server/run-server.sh
@@ -2,4 +2,4 @@
 
 export $(cat .env | xargs)
 
-java -jar -Dspring.profiles.active=product,mysql,private build/libs/server-1.0.0.jar
+java -jar -Dspring.profiles.active=product build/libs/server-1.0.0.jar
