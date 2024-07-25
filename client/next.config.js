396737a24648f18d0b17a95613a4181a313f7f98
jagaldol <yoy06056@pusan.ac.kr>
2024-07-25 15:47:17 +0900

    chore: 도메인 이전 반영

--- client/next.config.js
+++ client/next.config.js
@@ -5,7 +5,8 @@ const nextConfig = {
 
 module.exports = {
   images: {
-    domains: ["localhost", "chatfoodie.net"], // 이미지를 호스팅하는 도메인을 여기에 추가
+    domains: ["localhost", "chatfoodie.net", "foodie.jagaldol.com"], // 이미지를 호스팅하는 도메인을 여기에 추가
+    unoptimized: true,
   },
   ...nextConfig,
 }
